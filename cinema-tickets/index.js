b17fe0bbff9526ec997127fe42db1459caa0905c
burak <mbyilmazer38@gmail.com>
2024-11-11 20:36:11 +0300

    reserved ticket to local storage

--- cinema-tickets/index.js
+++ cinema-tickets/index.js
@@ -3,6 +3,7 @@ const count = document.getElementById("count");
 const amount = document.getElementById("amount");
 const select = document.getElementById("movie");
 const seats = document.querySelectorAll(".seat:not(.reserved)");
+const btn = document.querySelector(".btn");
 getFromLocalStorage();
 calculateTotal();
 
@@ -20,6 +21,33 @@ container.addEventListener("click", (e) => {
 select.addEventListener("change", (e) => {
   calculateTotal();
 });
+
+btn.addEventListener("click", (e) => {
+  calculateTotal();
+  let selectedSeats = container.querySelectorAll(".seat.selected");
+  //   console.log(selectedSeats);
+  let selectedSeatsArr = [];
+  let purchasedSeats = container.querySelectorAll(".seat.reserved");
+  //   console.log(purchasedSeats);
+  let purchasedSeatsArr = [];
+  purchasedSeats.forEach(function (seat) {
+    purchasedSeatsArr.push(seat);
+  });
+  console.log(purchasedSeatsArr);
+
+  selectedSeats.forEach(function (seat) {
+    selectedSeatsArr.push(seat);
+  });
+
+  if (selectedSeatsArr.length > 1) {
+    selectedSeatsArr.map((seat) => {
+      seat.classList.add("reserved");
+      seat.classList.remove("selected");
+      calculateTotal();
+    });
+  }
+});
+
 function calculateTotal() {
   let selectedSeats = container.querySelectorAll(".seat.selected");
 
@@ -50,6 +78,12 @@ function calculateTotal() {
 function saveToLocalStorage(indexs) {
   localStorage.setItem("selectedSeats", JSON.stringify(indexs));
   localStorage.setItem("selectedMovieIndex", select.selectedIndex);
+
+  // Satın alınan koltukların indekslerini kaydedin
+  const reservedSeats = [...document.querySelectorAll(".seat.reserved")].map(
+    (seat) => [...seats].indexOf(seat)
+  );
+  localStorage.setItem("reservedSeats", JSON.stringify(reservedSeats));
 }
 
 //? Get Data From LOCAL Storage
@@ -59,6 +93,14 @@ function getFromLocalStorage() {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
         seat.classList.add("selected");
+      }
+    });
+  }
+  const reservedSeats = JSON.parse(localStorage.getItem("reservedSeats"));
+  if (reservedSeats != null && reservedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (reservedSeats.indexOf(index) > -1) {
+        seat.classList.add("reserved");
       }
     });
   }
